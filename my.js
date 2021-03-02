be822a98bbb82b5d68216e2fc3a03bb8ecaa049e
samod singh <samod0098@gmail.com>
2021-03-03 04:25:27 +0530

    for loop

---
+++ my.js
@@ -0,0 +1,14 @@
+for( i =0; i<5; i++)
+{
+    document.write(i + "<br>"); 
+
+}
+document.write("<br> Res t of the code")
+document.write("<br>")
+var i =0;
+for (; i<5 ; i++)
+{
+    document.write(i + "<br>");
+
+}
+document.write("<br>Rest lt the code")
